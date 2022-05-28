90f447ac859bc47f10876b79468c998415bf55f5
chassis <38101674+Code-Ebullient@users.noreply.github.com>
2022-05-28 04:17:41 +0100

    Update user_data.php

--- reg_form/user_data.php
+++ reg_form/user_data.php
@@ -20,53 +20,24 @@ fwrite($file, $country . "\n");//Takes the inputted info on country input
 fclose($file);
 
 
-print_r (file_get_contents("userdata.csv"));
+//print_r (file_get_contents("userdata.csv"));
+$file_to_read = fopen('userdata.csv', 'r');
+ 
+if($file_to_read !== FALSE){
+     
+    echo "<table>\n";
+    while(($data = fgetcsv($file_to_read, 100, ',')) !== FALSE){
+        echo "<tr>";
+        for($i = 0; $i < count($data); $i++) {
+            echo "<td>".$data[$i]."</td>";
+        }
+        echo "</tr>\n";
+    }
+    echo "</table>\n";
+ 
+    fclose($file_to_read);
+}
 
-
-
-
-// $name = $_POST ['name'];
-// $email = $_POST ['email'];
-// $dob = $_POST ['dob'];
-// $gender = $_POST ['gender'];
-// $country = $_POST ['country'];
-
-// echo "Name: " . $name . "<br>";
-// echo "Email: " . $email . "<br>";
-// echo "Date_Of_Birth: " . $dob . "<br>";
-// echo "Gender: " . $gender . "<br>";
-// echo "Country: " . $country ;
-
-// $filename = "./userdata.csv";
-// $detail = fopen($filename, 'r+'); //opens the file in read mode
-// $contents = fread($detail, filesize($filename)); //read the open file
-
-// print_r($contents);// Prints the content of the file with an index
-// fclose($detail);//Close the file
-
-
-// $json = file_get_contents("userdata.csv");//Reads JSON file
-
-// $json_data = json_decode($json, true);//Decode JSON file
-
-// print_r($json_data);//Prints JSON data
-
-// if(isset($_POST['submit'])){
-//     $name = "Name: ".$_POST['name']." ";
-//     $email = "Email: ".$_POST['email']." ";
-//     $dob = "Date_Of_Birth: ".$_POST['dob']." ";
-//     $gender = "Gender: ".$_POST['gender']." ";
-//     $country = "Country: ".$_POST['country']." ";
-
-//     //$filename = "./userdata.csv";
-//     $file=fopen("file.txt", "a");
-//     fwrite($file, $name);
-//     fwrite($file, $email);
-//     fwrite($file, $dob);
-//     fwrite($file, $gender);
-//     fwrite($file, $country);
-//     fclose($file);
-// }
 
 ?>
 
